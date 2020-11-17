c8e7a4a517f0e84f427140d9f30e56d6387ffcac
Sakiskid <tylerdrew.home@gmail.com>
2020-11-17 13:25:35 -0600

    put and post routes are complete

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -12,4 +12,33 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.post("/api/workouts", (req, res) => {
+    console.log("posting to workouts");
+
+    db.Workout.create({})
+    .then(newWorkout => {
+        console.log("new workout: ", newWorkout);
+        res.json(newWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+})
+
+router.put("/api/workouts/:id", ({params, body}, res) => {
+    console.log("putting with id: ", params.id);
+
+    db.Workout.findByIdAndUpdate(
+        { _id: params.id },
+        { $push: { exercises: body } },
+    )
+    .then(newWorkout => {
+        res.json(newWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+
+})
+
 module.exports = router;
